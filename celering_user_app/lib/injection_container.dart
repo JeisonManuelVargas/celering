7e2be56b1aa567fcc81f2ae2195d939a59ab45f0
JEISONVARGAS1 <jeisonvargas@flutterlabapps.com>
2022-05-19 16:17:17 -0500

    nivelando rama

--- celering_user_app/lib/injection_container.dart
+++ celering_user_app/lib/injection_container.dart
@@ -1,5 +1,3 @@
-import 'package:celering_user_app/features/confirm_email/presentation/bloc/confirm_email_cubit.dart';
-import 'package:celering_user_app/features/register/presentation/bloc/register_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,8 +5,10 @@ import 'package:celering_user_app/amplifyconfiguration.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:celering_user_app/features/home/presentation/bloc/home_cubit.dart';
 import 'package:celering_user_app/features/login/presentation/bloc/login_cubit.dart';
+import 'package:celering_user_app/features/register/presentation/bloc/register_cubit.dart';
 import 'package:celering_user_app/features/search_view/presentation/bloc/search_view_cubit.dart';
 import 'package:celering_user_app/features/search_view/domain/use_cases/search_view_use_case.dart';
+import 'package:celering_user_app/features/confirm_email/presentation/bloc/confirm_email_cubit.dart';
 import 'package:celering_user_app/features/search_view/data/data_sources/search_view_data_sources.dart';
 import 'package:celering_user_app/features/search_view/domain/repositories/search_view_repository.dart';
 import 'package:celering_user_app/features/search_view/data/repositories/search_view_repository_impl.dart';
